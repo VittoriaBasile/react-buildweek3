a8971d68121ca07f2e544608cdaa9cc0803f6083
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-17 17:02:16 +0200

    edit merge componenti

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
     <div className="App">
       <Topbar />
       <ProfileMainPage />
-      <HeaderProfile />
       <MyFooter />
     </div>
   );
--- src/components/ProfileMainPage.jsx
+++ src/components/ProfileMainPage.jsx
@@ -2,6 +2,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { allProfileFetchAction, profileFetchAction } from "../redux/actions";
+import HeaderProfile from "./HeaderProfile";
 
 const ProfileMainPage = () => {
     
@@ -20,35 +21,23 @@ const ProfileMainPage = () => {
         return(
                 profile && (
                     <>
-                    {/* inserire qui navbar */}
-                            <Container fluid className="text-center bg-warning mb-4">
-                                    ciao
-                            </Container>
                             <Container fluid className="bg-mainpage">
                                     <Row className="bg-mainpage px-5 gx-2 justify-content-center">
-                                    <Col md={6} className="bg-danger mx-2">
+                                    <Col md={6} className="mx-2">
                     {/* sezione principale con profile header, inserire qui a cascata i vari componenti */}
-                                    componenti principali profilo <br/>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
-                                    Natus, laboriosam facilis error sed, amet tenetur labore est deleniti, 
-                                    eius provident vitae illo ratione velit maxime perferendis? 
-                                    Tenetur voluptatum nobis vitae! Card con {profile.name}
+                    <HeaderProfile profileData={profile}/>
                                     </Col>
-                                    <Col md={2} className="bg-success mx-2 d-none d-md-block">
+                                    <Col md={2} className="mx-2 d-none d-md-block">
                     {/* sezione secondaria, notizie e suggerimenti su sidebar, inserire qui a cascata i vari componenti */}
-                                    sidebar profilo <br/>
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, 
-                                    inventore fuga optio tempore illo blanditiis itaque accusamus nostrum? 
-                                    Voluptatibus consequuntur maiores porro odit repudiandae culpa vel enim beatae minus earum!
                                     {/* qui sotto il map che pesca tutti i profili */}
-                                    {/* {allProfile &&(
-                                        allProfile.map((profile) => {
+                                    {allProfile &&(
+                                        (allProfile.slice(0,5)).map((profile) => {
                                             return(
                                                 <p>{profile.name}</p>
                                             )
                                             
                                         }))
-                                    } */}
+                                    }
                                     </Col>
                                 </Row>
                             </Container>
