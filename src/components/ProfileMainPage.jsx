9f8230eabe52f7029fb81583049ff0b826ba6ffb
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-17 12:49:53 +0200

    short circuit ok

--- src/components/ProfileMainPage.jsx
+++ src/components/ProfileMainPage.jsx
@@ -32,29 +32,31 @@ const ProfileMainPage = () => {
     },[])
 
         return(
-                <>
-            {/* inserire qui navbar */}
-            <Container fluid className="text-center bg-warning mb-4">
-                ciao
-            </Container>
-            <Container fluid className="bg-mainpage">
-                <Row className="bg-mainpage px-5 gx-2 justify-content-center">
-                <Col md={6} className="bg-danger mx-2">
-                {/* sezione principale con profile header, inserire qui a cascata i vari componenti */}
-                componenti principali profilo <br/>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
-                {/* <img src={profile.image} alt="profile image" /> */}
-                Natus, laboriosam facilis error sed, amet tenetur labore est deleniti, eius provident vitae illo ratione velit maxime perferendis? Tenetur voluptatum nobis vitae! Card con {profile.name}
-                </Col>
-                <Col md={2} className="bg-success mx-2 d-none d-md-block">
-                {/* sezione secondaria, notizie e suggerimenti su sidebar, inserire qui a cascata i vari componenti */}
-                sidebar profilo <br/>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, inventore fuga optio tempore illo blanditiis itaque accusamus nostrum? Voluptatibus consequuntur maiores porro odit repudiandae culpa vel enim beatae minus earum!
-                </Col>
-            </Row>
-            </Container>
-            
-            </>
+                
+                profile && (
+                    <>
+                    {/* inserire qui navbar */}
+                            <Container fluid className="text-center bg-warning mb-4">
+                                    ciao
+                            </Container>
+                            <Container fluid className="bg-mainpage">
+                                    <Row className="bg-mainpage px-5 gx-2 justify-content-center">
+                                    <Col md={6} className="bg-danger mx-2">
+                    {/* sezione principale con profile header, inserire qui a cascata i vari componenti */}
+                                    componenti principali profilo <br/>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
+                                    
+                                    Natus, laboriosam facilis error sed, amet tenetur labore est deleniti, eius provident vitae illo ratione velit maxime perferendis? Tenetur voluptatum nobis vitae! Card con {profile.name}
+                                    </Col>
+                                    <Col md={2} className="bg-success mx-2 d-none d-md-block">
+                    {/* sezione secondaria, notizie e suggerimenti su sidebar, inserire qui a cascata i vari componenti */}
+                                    sidebar profilo <br/>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, inventore fuga optio tempore illo blanditiis itaque accusamus nostrum? Voluptatibus consequuntur maiores porro odit repudiandae culpa vel enim beatae minus earum!
+                                    </Col>
+                                </Row>
+                            </Container>
+                    </>
+                )
             )
 };
 export default ProfileMainPage;
