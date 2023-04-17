3617ae4c8749a8e125ec2f0419ee9b056de3aa0a
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-17 11:08:04 +0200

    store quasi ok

---
+++ src/redux/store/index.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+const persistConfig = {
+	key: "root",
+	storage,
+	transforms: [
+		encryptTransform({
+			secretKey1: process.env.REACT_APP_API_KEY,
+			secretKey2: process.env.REACT_APP_SECRET_KEY,
+		}),
+	],
+};
+
+const rootReducer = combineReducers({
+	// red1: ;
+	// red2: ;
+	// red3: ;
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
