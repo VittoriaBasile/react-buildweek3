89b2f7a3e4fbf59c2bb730954d7a79434477540d
VittoriaBasile <vichy.basile@gmail.com>
2023-04-17 22:03:22 +0200

    commit

--- src/Components/HeaderProfile.jsx
+++ src/Components/HeaderProfile.jsx
@@ -3,8 +3,9 @@ import { Button, Card, Carousel, Col, Row } from "react-bootstrap";
 const HeaderProfile = ({ profileData }) => {
   return (
     <>
-      <Card className="border border-secondary">
-        <Card.Img className="profile-background-image bg-secondary" variant="top" src="holder.js/100px180" />
+      <Card className="headerCard border mt-4">
+        <Card.Img className="profile-background-image" variant="top" src="holder.js/100px180" />
+
         <Card.Body>
           <Row>
             <Col>
@@ -23,14 +24,41 @@ const HeaderProfile = ({ profileData }) => {
               </Card.Text>
               <span className="text-secondary fs-6">{profileData.area}</span>
               <span>-</span>
-              <span className="text-primary fs-6">Informazioni di contatto: {profileData.email}</span>
+              <span className="info fs-6">Informazioni di contatto: {profileData.email}</span>
             </Col>
             <Col className="ms-5">
+              <div className="iconaFotoDiv position-absolute ">
+                <button className="modificaFoto border-0 bg-light rounded-circle ">
+                  <svg
+                    fill="#0a66c2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 3 16 16"
+                    data-supported-dps="16x16"
+                    class="mercado-match"
+                    width="16"
+                    height="16"
+                    focusable="false"
+                  >
+                    <path d="M10 9a2 2 0 11-2-2 2 2 0 012 2zm5-2.5V14H1V6.5A2.5 2.5 0 013.5 4h.75L5 2h6l.75 2h.75A2.5 2.5 0 0115 6.5zM11 9a3 3 0 10-3 3 3 3 0 003-3z"></path>
+                  </svg>
+                </button>
+              </div>
               <Row className="">
                 <div className="d-flex justify-content-end">
-                  <Button className="border-0 bg-transparent text-secondary" style={{ width: "50px" }}>
-                    svg
-                  </Button>
+                  <button className="btnModificaProfilo border-0 rounded-circle ">
+                    <svg
+                      fill="#666666"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      data-supported-dps="24x24"
+                      class="mercado-match"
+                      width="24"
+                      height="24"
+                      focusable="false"
+                    >
+                      <path d="M21.13 2.86a3 3 0 00-4.17 0l-13 13L2 22l6.19-2L21.13 7a3 3 0 000-4.16zM6.77 18.57l-1.35-1.34L16.64 6 18 7.35z"></path>
+                    </svg>
+                  </button>
                 </div>
               </Row>
 
@@ -41,74 +69,119 @@ const HeaderProfile = ({ profileData }) => {
               </Row>
             </Col>
           </Row>
-          <p className="text-primary fs-6">100 collegamenti</p>
-          <Row>
-            <Col xs={3} className="text-center p-0">
-              <Button variant="primary" className="rounded-pill">
-                Disponibile per
-              </Button>
-            </Col>
-            <Col xs={4} className="text-start p-0">
-              <Button variant="light" className="text-primary border border-primary rounded-pill">
-                Aggiungi sezione del profilo
-              </Button>
-            </Col>
-            <Col xs={2} className="text-start p-0">
-              <Button variant="light" className="text-secondary border border-secondary rounded-pill">
-                Altro
-              </Button>
-            </Col>
-          </Row>
+          <p className="info fs-6">100 collegamenti</p>
+          <div>
+            <button className="btnDisponibile border-0 text-light px-3 me-1 fs-6 rounded-pill fw-semibold">
+              Disponibile per
+            </button>
+
+            <button className="btnAdd info bg-light me-1 fs-6 px-3 rounded-pill fw-semibold">
+              Aggiungi sezione del profilo
+            </button>
+
+            <button className="bg-light  px-3 fs-6 text-secondary border border-secondary rounded-pill fw-semibold">
+              Altro
+            </button>
+          </div>
           <Carousel>
             <Row className="mt-3 flex-nowrap overflow-auto">
-              <Card className="d-flex flex-row" style={{ width: "370px", backgroundColor: "#dce6f1" }}>
+              <Card className="miniHeaderCard cardDettaglio d-flex flex-row mx-3">
                 <Card.Body className="hl-1">
-                  <Col xs={9} className="">
-                    <h3 className="fs-6">Disponibile a lavorare</h3>
+                  <div className="lh-sm">
+                    <span className="fs-6 fw-semibold">Disponibile a lavorare</span>
                     <p className="mb-2  fs-6">lavoro</p>
-                    <Card.Link href="#">Mostra dettagli</Card.Link>
-                  </Col>
+                    <Card.Link className="linkCard info " href="#">
+                      Mostra dettagli
+                    </Card.Link>
+                  </div>
                 </Card.Body>
 
-                <Col xs={3} style={{ width: "10px" }}>
+                <div style={{ width: "10px" }}>
                   <div className="d-flex justify-content-end">
-                    <Button className="border-0 bg-transparent text-secondary">svg</Button>
+                    <Button className="border-0 bg-transparent text-secondary">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        data-supported-dps="16x16"
+                        fill="#585c60"
+                        class="mercado-match"
+                        width="16"
+                        height="16"
+                        focusable="false"
+                      >
+                        <path d="M14.13 1.86a3 3 0 00-4.17 0l-7 7L1 15l6.19-2 6.94-7a3 3 0 000-4.16zm-8.36 9.71l-1.35-1.34L9.64 5 11 6.35z"></path>
+                      </svg>
+                    </Button>
                   </div>
-                </Col>
+                </div>
               </Card>
 
-              <Card className="d-flex flex-row" style={{ width: "370px" }}>
+              <Card className="miniHeaderCard d-flex flex-row mx-2">
                 <Card.Body className="hl-1">
-                  <Col xs={9} className="">
-                    <span className="fs-6 fw-bold">Metti in risalto i servizi </span>
+                  <div className="lh-sm">
+                    <span className="fs-6 fw-semibold">Metti in risalto i servizi </span>
                     <span className="mb-2  fs-6">
                       che offri, così tu e la tua azienda potrete apparire nei risultati di ricerca.
                     </span>
-                    <Card.Link href="#">Inizia</Card.Link>
-                  </Col>
+                    <div>
+                      <Card.Link className="linkCard info " href="#">
+                        Inizia
+                      </Card.Link>
+                    </div>
+                  </div>
                 </Card.Body>
 
-                <Col xs={3} style={{ width: "10px" }}>
+                <div style={{ width: "10px" }}>
                   <div className="d-flex justify-content-end">
-                    <Button className="border-0 bg-transparent text-secondary">X</Button>
+                    <Button className="border-0 bg-transparent text-secondary">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        data-supported-dps="16x16"
+                        fill="currentColor"
+                        class="mercado-match"
+                        width="16"
+                        height="16"
+                        focusable="false"
+                      >
+                        <path d="M14 3.41L9.41 8 14 12.59 12.59 14 8 9.41 3.41 14 2 12.59 6.59 8 2 3.41 3.41 2 8 6.59 12.59 2z"></path>
+                      </svg>
+                    </Button>
                   </div>
-                </Col>
+                </div>
               </Card>
 
-              <Card className="d-flex flex-row" style={{ width: "370px" }}>
-                <Card.Body className="hl-1">
-                  <Col xs={9} className="">
-                    <span className="fs-6 fw-bold">Fai sapere che stai facendo selezione </span>
-                    <span className="mb-2  fs-6">e attrai candidati qualificati. </span>
-                    <Card.Link href="#">Inizia</Card.Link>
-                  </Col>
+              <Card className="miniHeaderCard d-flex flex-row mx-3">
+                <Card.Body className="">
+                  <div className="lh-sm">
+                    <span className="fs-6 fw-semibold">Fai sapere che stai facendo selezione </span>
+                    <span className="mb-2 fs-6">e attrai candidati qualificati. </span>
+                    <div>
+                      <Card.Link className="linkCard info" href="#">
+                        Inizia
+                      </Card.Link>
+                    </div>
+                  </div>
                 </Card.Body>
 
-                <Col xs={3} style={{ width: "10px" }}>
+                <div style={{ width: "10px" }}>
                   <div className="d-flex justify-content-end">
-                    <Button className="border-0 bg-transparent text-secondary">X</Button>
+                    <Button className="border-0 bg-transparent text-secondary">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        data-supported-dps="16x16"
+                        fill="currentColor"
+                        class="mercado-match"
+                        width="16"
+                        height="16"
+                        focusable="false"
+                      >
+                        <path d="M14 3.41L9.41 8 14 12.59 12.59 14 8 9.41 3.41 14 2 12.59 6.59 8 2 3.41 3.41 2 8 6.59 12.59 2z"></path>
+                      </svg>
+                    </Button>
                   </div>
-                </Col>
+                </div>
               </Card>
             </Row>
           </Carousel>
