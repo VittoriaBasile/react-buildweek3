8927fcf1de6a43fd78734fcd44b9972e10e6dc66
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-04-17 16:04:27 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Topbar from "./components/Topbar";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <MyFooter />
     </div>
   );
 }
