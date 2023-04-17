e8f9d393eb899775b000e9f9db6d6ad5583e2d71
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-17 12:09:50 +0200

    edit comments profile page

--- src/components/ProfileMainPage.jsx
+++ src/components/ProfileMainPage.jsx
@@ -8,13 +8,13 @@ const ProfileMainPage = () => {
                 ciao
             </Container>
             <Row className="bg-secondary px-5 gx-2 justify-content-center">
-                <Col md={6} className="bg-danger mx-2">
-                {/* sezione principale con profile header */}
+                <Col md={7} className="bg-danger mx-2">
+                {/* sezione principale con profile header, inserire qui a cascata i vari componenti */}
                 componenti principali profilo <br/>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, laboriosam facilis error sed, amet tenetur labore est deleniti, eius provident vitae illo ratione velit maxime perferendis? Tenetur voluptatum nobis vitae!
                 </Col>
-                <Col md={2} className="bg-success mx-2 d-none d-md-block">
-                {/* sezione secondaria, notizie e suggerimenti su sidebar */}
+                <Col md={3} className="bg-success mx-2 d-none d-md-block">
+                {/* sezione secondaria, notizie e suggerimenti su sidebar, inserire qui a cascata i vari componenti */}
                 sidebar profilo <br/>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, inventore fuga optio tempore illo blanditiis itaque accusamus nostrum? Voluptatibus consequuntur maiores porro odit repudiandae culpa vel enim beatae minus earum!
                 </Col>
