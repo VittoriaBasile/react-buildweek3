c8bef6c1fe587ee8a8df48907d47abb697069da8
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-17 12:45:31 +0200

    edit spacings layout

--- src/components/ProfileMainPage.jsx
+++ src/components/ProfileMainPage.jsx
@@ -37,20 +37,23 @@ const ProfileMainPage = () => {
             <Container fluid className="text-center bg-warning mb-4">
                 ciao
             </Container>
-            <Row className="bg-secondary px-5 gx-2 justify-content-center">
-                <Col md={7} className="bg-danger mx-2">
+            <Container fluid className="bg-mainpage">
+                <Row className="bg-mainpage px-5 gx-2 justify-content-center">
+                <Col md={6} className="bg-danger mx-2">
                 {/* sezione principale con profile header, inserire qui a cascata i vari componenti */}
                 componenti principali profilo <br/>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
                 {/* <img src={profile.image} alt="profile image" /> */}
                 Natus, laboriosam facilis error sed, amet tenetur labore est deleniti, eius provident vitae illo ratione velit maxime perferendis? Tenetur voluptatum nobis vitae! Card con {profile.name}
                 </Col>
-                <Col md={3} className="bg-success mx-2 d-none d-md-block">
+                <Col md={2} className="bg-success mx-2 d-none d-md-block">
                 {/* sezione secondaria, notizie e suggerimenti su sidebar, inserire qui a cascata i vari componenti */}
                 sidebar profilo <br/>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, inventore fuga optio tempore illo blanditiis itaque accusamus nostrum? Voluptatibus consequuntur maiores porro odit repudiandae culpa vel enim beatae minus earum!
                 </Col>
             </Row>
+            </Container>
+            
             </>
             )
 };
