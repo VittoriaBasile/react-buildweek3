372805d7f8fb12455bab09cccc3ba1a4bc2d93a9
VittoriaBasile <vichy.basile@gmail.com>
2023-04-17 11:35:43 +0200

    commit

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -4,27 +4,27 @@ import storage from "redux-persist/lib/storage";
 import { encryptTransform } from "redux-persist-transform-encrypt";
 
 const persistConfig = {
-	key: "root",
-	storage,
-	transforms: [
-		encryptTransform({
-			secretKey1: process.env.REACT_APP_API_KEY,
-			secretKey2: process.env.REACT_APP_SECRET_KEY,
-		}),
-	],
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey1: process.env.REACT_APP_API_KEY,
+      secretKey2: process.env.REACT_APP_SECRET_KEY,
+    }),
+  ],
 };
 
 const rootReducer = combineReducers({
-	// red1: ;
-	// red2: ;
-	// red3: ;
+  // red1: ;
+  // red2: ;
+  // red3: ;
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: persistedReducer,
-	middleware: getDefaultMiddleware({ serializableCheck: false }),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export const persistor = persistStore(store);
