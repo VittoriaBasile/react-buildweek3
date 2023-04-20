6d616491d22b3f4eed9e8a1e92606c86e2994bfe
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-20 14:37:49 +0200

    edit style e commenti

--- src/Components/SingleExperience.jsx
+++ src/Components/SingleExperience.jsx
@@ -39,11 +39,6 @@ const SingleExperience =({experience}) => {
                   <path d="M21.13 2.86a3 3 0 00-4.17 0l-13 13L2 22l6.19-2L21.13 7a3 3 0 000-4.16zM6.77 18.57l-1.35-1.34L16.64 6 18 7.35z"></path>
                 </svg>
               </button>
-                {/* <Button 
-                variant="danger" 
-                onClick={handleDelete}>
-                Elimina
-                </Button> */}
             </Col>
           </Row>
           <div className="px-3">
