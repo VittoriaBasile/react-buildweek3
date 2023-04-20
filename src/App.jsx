3d1f31658550e70ceedfaa8593198a334887067c
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-04-20 17:06:02 +0200

    job-page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProfileMainPage from "./components/ProfileMainPage";
 import HomePage from "./components/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound";
+import JobsPage from "./components/JobsPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/profile/" element={<ProfileMainPage />} />
+        <Route path="/jobs/" element={<JobsPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
