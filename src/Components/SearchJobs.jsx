5a2e22edfd56ebda7a04efcf343febaa32948f63
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-04-21 11:25:06 +0200

    adj-job

--- src/Components/SearchJobs.jsx
+++ src/Components/SearchJobs.jsx
@@ -4,35 +4,64 @@ import { Link } from "react-router-dom";
 
 const SearchJobs = () => {
   const searchResults = useSelector((state) => state.searchJobs.content);
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <Row className="mx-1">
 
-            {/* <Col xs={1}>
+  return (
+    <Row>
+      <Col>
+        <h4 className="ps-3">Offerte di lavoro :</h4>
+        <Row className="mx-1">
+          {/* <Col xs={1}>
               <img style={{ width: "40px" }} src="https://picsum.photos/40" alt="" />
             </Col> */}
-            <Col xs={9} className="d-flex flex-column px-3 testoGray">
-              {searchResults.length > 0 && (searchResults.map((job)=> (
-                <>
-                <Card key={job._id} className="my-2">
-                  <Card.Header>{job.title}</Card.Header>
-                  <Card.Body>
-                    <Card.Title>{job.company_name}</Card.Title>
-                    <Card.Text>
-                      {job.category}
-                    </Card.Text>
-                    <Link to={job.url} variant="primary">{job.url}</Link>
-                  </Card.Body>
-                </Card>
-                </>
-              )))}
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+          <Col xs={12} className="d-flex flex-column  testoGray">
+            {searchResults.length > 0 &&
+              searchResults.map((job) => {
+                const date = new Date(job.publication_date);
+                const formattedDate = `${date.getDate()}-${
+                  date.getMonth() + 1
+                }-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+                console.log(formattedDate);
+
+                return (
+                  <>
+                    <Card key={job._id} className="my-2">
+                      <Card.Header className="fw-bold">{job.title}</Card.Header>
+                      <Card.Body>
+                        <Card.Title className="fw-bolder d-flex justify-content-between align-items-center">
+                          <div>{job.company_name}</div>
+                          <div>
+                            <Button
+                              className="btn-blue-custom rounded-pill border-primary text-primary fw-bold"
+                              variant="outlined"
+                            >
+                              Salva
+                            </Button>
+                          </div>
+                        </Card.Title>
+
+                        <Card.Text className="fw-semibold d-flex justify-content-between align-items-center">
+                          <div>{job.category}</div>
+                          <div>{job.job_type}</div>
+                          <div>{job.candidate_required_location}</div>
+                        </Card.Text>
+
+                        <Link
+                          to={job.url}
+                          variant="primary"
+                          className="fw-normal footer-icon-links"
+                        >
+                          <span>Visita il sito di {job.company_name}</span>
+                        </Link>
+                      </Card.Body>
+                      <div className="text-end p-1">{formattedDate}</div>
+                    </Card>
+                  </>
+                );
+              })}
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 };
 
