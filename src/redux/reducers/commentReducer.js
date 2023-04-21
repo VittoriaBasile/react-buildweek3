fbb4beea9eaaab65abc1f635b14876a17e08797e
VittoriaBasile <vichy.basile@gmail.com>
2023-04-21 11:22:31 +0200

    comments

--- src/redux/reducers/commentReducer.js
+++ src/redux/reducers/commentReducer.js
@@ -1,19 +1,19 @@
 import { COMMENTS } from "../actions";
 
 const initialState = {
-	content: null,
+  content: [],
 };
 
 const commentReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case COMMENTS:
-			return {
-				...state,
-				content: action.payload,
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case COMMENTS:
+      return {
+        ...state,
+        content: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default commentReducer;
