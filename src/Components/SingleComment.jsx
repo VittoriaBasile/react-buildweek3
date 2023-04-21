e8f388f48f81575575d2ef647d3f67532d2c4ae1
VittoriaBasile <vichy.basile@gmail.com>
2023-04-21 15:28:49 +0200

    input commento

--- src/Components/SingleComment.jsx
+++ src/Components/SingleComment.jsx
@@ -1,15 +1,13 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import {  getCommentAction, singleProfileFetchAction } from "../redux/actions"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCommentAction, singleProfileFetchAction } from "../redux/actions";
 
-const SingleComment =(props) => {
-
-    return(
-        <>
-        <p>{props.comment.author}</p>
-        <p className="border rounded p-2 m-1">{props.comment.comment && props.comment.comment}</p>
-        </>
-        
-    )
-}
-export default SingleComment
+const SingleComment = (props) => {
+  return (
+    <>
+      <p>{props.comment.author}</p>
+      <p className="border rounded p-2 m-1">{props.comment.comment && props.comment.comment}</p>
+    </>
+  );
+};
+export default SingleComment;
